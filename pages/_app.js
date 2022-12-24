af50463d756e750bfb7b948ea7890a7b9bc3d055
Rodrigo Luglio <rodrigo@rlabs.com.br>
2022-12-24 16:10:28 -0300

    merge

--- pages/_app.js
+++ pages/_app.js
@@ -7,45 +7,41 @@ import { MantineProvider, ColorSchemeProvider } from "@mantine/core";
 import { NotificationsProvider } from "@mantine/notifications";
 import PresenceTheme from "../services/presenceTheme";
 
-function MyApp({ Component, pageProps: { session, ...pageProps } }) {
-  
-    const [colorScheme, setColorScheme] = useState("light");
-    const toggleColorScheme = (value) =>
-        setColorScheme(value || (colorScheme === "dark" ? "light" : "dark"));
 
-function MyApp({ Component, pageProps: { session, ...pageProps } }) {
-    const [colorScheme, setColorScheme] = useState("light");
-    const toggleColorScheme = (value) =>
-        setColorScheme(value || (colorScheme === "dark" ? "light" : "dark"));
+    function MyApp({ Component, pageProps: { session, ...pageProps } }) {
+        const [colorScheme, setColorScheme] = useState("light");
+        const toggleColorScheme = (value) =>
+            setColorScheme(
+                value || (colorScheme === "dark" ? "light" : "dark")
+            );
 
-    const getLayout = Component.getLayout || ((page) => page);
+        const getLayout = Component.getLayout || ((page) => page);
 
-    return (
-        <>
-            <ColorSchemeProvider
-                colorScheme={colorScheme}
-                toggleColorScheme={toggleColorScheme}
-            >
-                <MantineProvider
-                        theme={ PresenceTheme(colorScheme) }
+        return (
+            <>
+                <ColorSchemeProvider
+                    colorScheme={colorScheme}
+                    toggleColorScheme={toggleColorScheme}
+                >
+                    <MantineProvider
+                        theme={PresenceTheme(colorScheme)}
                         withGlobalStyles
                         withNormalizeCSS
-                >
-                    {/* Quando for testar local descomentar a linha abaixo */}
-                    {/* <SessionProvider session={session}> */}
-                    {/* Quando for fazer push para o servidor descomentar a linha abaixo */}
-                    <SessionProvider
+                    >
+                        {/* Quando for testar local descomentar a linha abaixo */}
+                        <SessionProvider session={session}>
+                            {/* Quando for fazer push para o servidor descomentar a linha abaixo */}
+                            {/* <SessionProvider
                         session={session}
                         basePath="/indicadores/api/auth"
-                    >
+                    > */}
                             <NotificationsProvider position="top-right">
                                 {getLayout(<Component {...pageProps} />)}
                             </NotificationsProvider>
-                    </SessionProvider>
-                </MantineProvider>
-            </ColorSchemeProvider>
-        </>
-    );
-}
-
-export default MyApp;
+                        </SessionProvider>
+                    </MantineProvider>
+                </ColorSchemeProvider>
+            </>
+        );
+    }
+    export default MyApp;
