ef542346dfb9381b6efd248b8405fc5953c65a52
Iury Nadin <iury@noctuaart.com.br>
2022-12-29 16:27:47 -0300

    cadastro de cliente

--- services/clientes.js
+++ services/clientes.js
@@ -1,12 +1,15 @@
 import axios from "axios";
-import { slugify } from "./utils";
+import { slugify, generatePassword } from "./utils";
+// import qs from "qs";
 
 // const strapiUrl = process.env.STRAPI_URL;
 const strapiUrl = "https://api.rlabs.com.br";
 
 export async function getUsersByRole(jwt, role) {
     try {
-        const { data } = await axios.get(`${strapiUrl}/api/users?populate=role&filters[role][name][$eq]=${role}`, {
+        const org = `populate[organizacao][fields][0]=nome&`;
+        const roleq = `populate[role][fields][0]=name&`;
+        const { data } = await axios.get(`${strapiUrl}/api/users?fields[0]=name&fields[1]=email&${roleq + org}&filters[role][name][$eq]=${role}`, {
             headers: {
                 Authorization: `Bearer ${jwt}`,
             },
@@ -20,15 +23,18 @@ export async function getUsersByRole(jwt, role) {
 
 export async function addUpCliente(jwt, dados) {
     if(dados.id == null) {
-        const username = slugify(dados?.nome);
+        const password = generatePassword(12);
+        console.log('password', password);
+        console.log('dados: ', dados);
+        console.log('dados.nome: ', slugify(dados.nome));
         try {
             const res = await axios.post(`${strapiUrl}/api/users`, 
                 {
-                    username: username,
-                    password: 'axcd74rGG3821',
-                    role: 4,
+                    username: slugify(dados?.nome) + generatePassword(4),
+                    password: password,
+                    role: dados.role,
                     email: dados.email,
-                    nome: dados.nome
+                    name: dados.nome
                 },
                 {
                     headers: {
@@ -36,9 +42,42 @@ export async function addUpCliente(jwt, dados) {
                     }
                 },
             );
+
+            const user = res.data.id;
+            if(user) {
+                const resOrg = await axios.post(`${strapiUrl}/api/organizacoes`,
+                    {
+                        data: {
+                            nome: dados.organizacao,
+                            users: [user],
+                        }
+                    },
+                    {
+                        headers: {
+                            Authorization: `Bearer ${jwt}`,
+                        }
+                    }
+                )
+                console.log('resOrg', resOrg);
+            }
             return res.data;
         } catch (error) {
             return error;
         }
     }
 }
+
+
+
+// export async function getDepartamentos(jwt, role) {
+//     try {
+//         const { data } = await axios.get(`${strapiUrl}/api/departamentos?fields[0]=nome`, {
+//             headers: {
+//                 Authorization: `Bearer ${jwt}`,
+//             },
+//         });
+//         return data;
+//     } catch (error) {
+//         return error;
+//     }
+// }
--- services/utils.js
+++ services/utils.js
@@ -25,7 +25,7 @@ export const getStatusColor = (term, status) => {
 }
 
 
-export const slugify = (text, separator) => {
+export const slugify = (text, separator = '') => {
     return text.toString()
     .normalize('NFD')                   // split an accented letter in the base letter and the acent
     .replace(/[\u0300-\u036f]/g, '')   // remove all previously split accents
@@ -33,4 +33,56 @@ export const slugify = (text, separator) => {
     .trim()
     .replace(/[^a-z0-9 ]/g, '')   // remove all chars not letters, numbers and spaces (to be replaced)
     .replace(/\s+/g, separator);
+}
+
+export const generatePassword = (length) => {
+    var pwdChars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    return new Array(length).fill(0).map(x => (function(chars) { 
+        let umax = Math.pow(2, 32), r = new Uint32Array(1), max = umax - (umax % chars.length); 
+        do { crypto.getRandomValues(r); } 
+        while(r[0] > max); 
+        return chars[r[0] % chars.length]; 
+    })(pwdChars)).join('');
+}
+
+
+export const getDepartamentos = () => {
+    return [
+        'Administrativo', 
+        'Atacado', 
+        'Atendimento ao cliente', 
+        'Auditoria', 
+        'Comercial', 
+        'Comunicação', 
+        'Contabilidade', 
+        'Controladoria', 
+        'Crédito', 
+        'Desenvolvimento de negócios', 
+        'Estratégia', 
+        'Exportação', 
+        'Financeiro	', 
+        'Garantia de Qualidade', 
+        'Importação', 
+        'Inteligência de Mercado', 
+        'Legal', 
+        'Logística', 
+        'Manutenção', 
+        'Marketing', 
+        'Operações', 
+        'Pesquisa e Desenvolvimento	', 
+        'Planejamento', 
+        'Planejamento Financeiro', 
+        'Processos	', 
+        'Produção', 
+        'Projetos', 
+        'Recursos Humanos', 
+        'Seguros	', 
+        'Tesouraria', 
+        'TI – Tecnologia da Informação', 
+        'Treinamento e Desenvolvimento', 
+        'Tributário, Fiscal', 
+        'Varejo', 
+        'Vendas' 
+    ];
+
 }
