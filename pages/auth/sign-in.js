ac4e0745f1e950a43ab5c1ca10626e4718ea44ba
iury nadin <iurynadin@gmail.com>
2022-12-30 14:11:38 -0300

    alterado form de login para componentes mantine

--- pages/auth/sign-in.js
+++ pages/auth/sign-in.js
@@ -6,78 +6,96 @@ import { signIn } from "next-auth/react";
 import { useRouter } from "next/router";
 import logoLogin from '../../public/imgs/logo-login.svg';
 
-// import { TextInput,  Button} from '@mantine/core';
-// import { useForm } from '@mantine/form';
+
+import { TextInput,  Button, PasswordInput} from '@mantine/core';
+import { useForm } from '@mantine/form';
+import { Alert } from '@mantine/core';
+import { IconAlertCircle } from '@tabler/icons';
 
 export default function SignIn() {
-  const router = useRouter();
+    
+    const router = useRouter();
 
-  const onSubmit = async (e) => {
-    e.preventDefault();
-    const result = await signIn("credentials", {
-      redirect: false,
-      email: e.target.email.value,
-      password: e.target.password.value,
+    const loginform = useForm({
+        initialValues: {
+            email: "",
+            password: "",
+        },
+        validate: {
+            email: (value) => (/^\S+@\S+$/.test(value) ? null : 'Email inválido'),
+            password: (value) => (value.length < 2 ? 'Senha precisa ter pelo menos 2 dígitos' : null),
+        },
     });
-    if (result.ok) {
-      router.replace("/");
-      return;
-    }
-    alert("Credential is not valid");
-  };
 
-  return (
-    <>
-      <Head>
-        <title>Presence</title>
-      </Head>
-      <section className="loginpage">
+    const loginSubmit =  loginform.onSubmit(
+        async (values) =>  {
+            console.log('value', values);
+            const result = await signIn("credentials", {
+                redirect: false,
+                email: values.email,
+                password: values.password,
+            });
+            if (result.ok) {
+                router.replace("/");
+                return;
+            }
+            alert("Credential is not valid");
+        },
+        (errors) => console.log(errors)
+    );
 
-        <div className="loginpage__bgcol">
-          <Image src={logoLogin} className="" alt="logo Presence - Comunicação e Sustentabilidade"/>
-        </div>
 
-        {/* login box */}
-        <div className="loginbox">
+    return (
+      <>
+        <Head>
+          <title>Presence</title>
+        </Head>
+        <section className="loginpage">
 
-            <div className="loginbox__bar"></div>
+            {/* <Alert icon={<IconAlertCircle size={16} />} title="Email ou senha incorreta!!" color="red" variant="filled">
+                Dados nã conferem com os nossos registros. Caso tenha esquecido seus dados clique abaixo no "esqueci minha senha"
+            </Alert> */}
 
-            <div className="loginbox__content">
-
-              <div className="loginbox__wrapper">
-                
-                <h1>Faça seu login</h1>
-
-                <form onSubmit={onSubmit} autoComplete="off">
-
-                  <label htmlFor="email">Email</label>
-                  <input 
-                    id="email" 
-                    name="email" 
-                    type="text" 
-                    autoComplete="nope" />
-
-                  <label htmlFor="password">Senha</label>
-                  <input
-                    id="password"
-                    name="password"
-                    type="password"
-                    autoComplete="new-password"
-                  />
-                  
-                  <div>
-                    <button>Entrar</button>
-                    <Link href="/redefinir-senha">Esqueci minha senha</Link>
-                  </div>
-
-                </form>
-              </div>
+            <div className="loginpage__bgcol">
+                <Image src={logoLogin} className="" alt="logo Presence - Comunicação e Sustentabilidade"/>
             </div>
-        </div>
 
-      </section>
-    </>
+            {/* login box */}
+            <div className="loginbox">
+                <div className="loginbox__bar"></div>
+                <div className="loginbox__content">
+                    <div className="loginbox__wrapper">
+
+                        <h1>Faça seu login</h1>
+
+                        <form onSubmit={loginSubmit}>
+
+                            <TextInput
+                                withAsterisk
+                                label="Email"
+                                {...loginform.getInputProps("email")}
+                            />
+
+                            <PasswordInput
+                                withAsterisk
+                                label="Senha"
+                                {...loginform.getInputProps("password")}
+                                bg="rgba(255,255,255,0)"
+                            />
+                            
+                            <div className="flex flex-col sm:flex-row sm:justify-between items-start sm:items-center space-y-4 sm:space-y-0 my-4 ">
+                                <Button type="submit">Entrar</Button>
+                                <Link href="/redefinir-senha">Esqueci minha senha</Link>
+                            </div>
+
+                        </form>
+                    </div>
+                </div>
+            </div>
+
+        </section>
+      </>
 
 
-  );
+    );
 }
